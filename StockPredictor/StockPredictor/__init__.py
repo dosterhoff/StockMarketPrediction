454204fb6c0028dd43fb8c438b48ed085021d966
Dave <dosterhoff@oakland.edu>
2019-09-18 20:18:54 -0400

    attempt to add login

--- StockPredictor/StockPredictor/__init__.py
+++ StockPredictor/StockPredictor/__init__.py
@@ -3,6 +3,12 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 app = Flask(__name__)
 
-import StockPredictor.views
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///3'
+
+#import StockPredictor.views
